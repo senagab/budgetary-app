8f3332c558e5644c1524207015165d6d0b71661a
senagab <senagab@gmail.com>
2025-03-26 21:45:09 -0300

    feat: main navigation component + home button, delete user button + heroicons library import

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,6 +4,9 @@ import { Outlet, useLoaderData } from "react-router-dom";
 // assets
 import wave from "../assets/wave.svg"
 
+// components
+import Nav from "../components/Nav";
+
 // helper functions
 import { fetchData } from "../helpers"
 
@@ -18,7 +21,7 @@ const Main = () => {
 
     return (
         <div className="layout">
-            <h1>Main</h1>
+            <Nav userName={userName} /> {/* passing userName as a prop */}
             <main>
                 <Outlet />
             </main>
