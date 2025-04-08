a31c50838c1525722b1e82f1c3c9d7fb80b24c4b
senagab <senagab@gmail.com>
2025-04-07 22:37:40 -0300

    feat: custom hooks for navigating function

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,30 @@
-import { useRouteError } from "react-router-dom"
+import { useRouteError, Link, useNavigate } from "react-router-dom"
+
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/16/solid"
 
 const Error = () => {
     const error = useRouteError() // custom hook to get the error object
+    const navigate = useNavigate() // custom hook to get the navigate function
     
-
     return (
         <div className="error">
             <h1>Uh oh! We've got a problem</h1>
             <p>{error.message || error.statusText}</p>
+            <div className="flex-md">
+                <button className="btn btn--dark" 
+                onClick={() => navigate(-1)}>
+                    <ArrowUturnLeftIcon width={20}/>
+                    <span>Go back</span>
+                </button>
+                <Link
+                to="/"
+                className="btn btn--dark"
+                >
+                    <HomeIcon width={20}/>
+                    <span>Go home</span>
+                </Link>
+            </div>
         </div>
     )
 }
